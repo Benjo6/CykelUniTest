551f3923dc1950034df1bfeee171554996abfba7
Benjo6 <58556176+Benjo6@users.noreply.github.com>
2020-09-24 14:49:49 +0200

    Add project files.

---
+++ Cykel/Cykel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Cykel
+{
+    public class Cykel
+    {
+
+        private int _id;
+        private string _farve;
+        private int _pris;
+        private int _gear;
+
+        public Cykel(string farve, int pris, int gear, int id)
+        {
+            Id = id;
+            Pris = pris;
+            Farve = farve;
+            Gear = gear;
+
+        }
+
+        public int Id { get => _id; private set => _id = value; }
+
+        public int Pris
+        {
+            get => _pris; set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException("LOOOOOL");
+                _pris = value;
+            }
+        }
+        public string Farve
+        {
+            get => _farve; set
+            {
+                if (value == null) throw new ArgumentNullException("Why are you making me mad");
+                if (value.Length < 1) throw new ArgumentException("STUPID");
+
+                _farve = value;
+            }
+        }
+        public int Gear
+        {
+            get => _gear;
+
+            set
+            {
+                if (3 <= value && value <= 32)
+                    _gear = value;
+                else throw new ArgumentOutOfRangeException("gear", value, "Oh Hell No");
+            }
+        }
+    }
+}
---
+++ CykelTests/CykelTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cykel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cykel.Tests
+{
+    [TestClass()]
+    public class CykelTests
+    {
+        private Cykel c = new Cykel("Blå", 10, 10, 1);
+        [TestMethod()]
+        public void CykelTestConstructor()
+        {
+            Cykel st = new Cykel("Rød", pris: 11, gear: 11, id: 2);
+            Assert.AreEqual("Blå", c.Farve);
+            Assert.AreEqual(10, c.Pris);
+            Assert.AreEqual(10, c.Gear);
+            Assert.AreEqual(1, c.Id);
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestPrisLav()
+        {
+            c.Pris = 0;
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGearLav()
+        {
+            c.Gear = 2;
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGearHøj()
+        {
+            c.Gear = 33;
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestFarveNull()
+        {
+            c.Farve = null;
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestFarveKort()
+        {
+            c.Farve = "";
+        }
+    }
+}
